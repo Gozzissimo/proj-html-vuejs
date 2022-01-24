36bbf58edcabee370f700138d45c0c460502df03
Gozzissimo <fantini.nico@gmail.com>
2022-01-24 15:46:04 +0100

    header-top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div></div>
+    <footer></footer>
 </template>
 
 <script>
@@ -9,5 +9,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,33 @@
 <template>
-    <div></div>
+    <header>
+        <div class="cont-70">
+            <div class="header-top container py-2">
+                <div class="row">
+                    <div class="col-6">
+                        <i class="fas fa-clock"></i>
+                        Open Hours: Mon - Sat - 9:00 - 18:00
+                    </div>
+                    <div class="col-2 text-end">
+                        <i class="fas fa-phone-alt"></i>
+                        +1 (305) 1234-5678
+                    </div>
+                    <div class="col-2 text-end">
+                        <i class="fas fa-envelope"></i>
+                        hello@example.com
+                    </div>
+                    <div class="col-2 text-end social">
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-linkedin-in"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="header-bottom container">
+                <nav></nav>
+                <div class="jumbotron container"></div>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -9,5 +37,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    header {
+        background-color: #32414e;
+        i {
+            margin: 0 5px;
+        }
+        .header-top {
+            color: #b3b3b8;
+            .social i {
+                margin: 0 15px;
+            }
+        }
+    }
 </style>
